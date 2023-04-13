d5d5981ea0ea2054c7ea497b7a2133fb06e0bb32
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-04-13 16:09:51 +0700

    first commit

---
+++ app/src/main/java/com/example/sicupi/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.sicupi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                finish();
+
+            }
+        }, 1400L);
+    }
+}
---
+++ app/src/main/java/com/example/sicupi/util/DataApi.java
@@ -0,0 +1,22 @@
+package com.example.sicupi.util;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class DataApi {
+
+    public static final String IP_ADDRESS ="";// ip address
+    public static final String BASE_URL ="http://" + IP_ADDRESS+"/pegawai/api/";
+
+    public static Retrofit retrofit = null;
+    public static Retrofit getClient(){
+        if (retrofit == null) {
+            retrofit  = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
+}
